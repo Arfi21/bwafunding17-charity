252a8644a5f53f15f239928f9124c92fe83a4c46
Arfi21 <magangarfianadm@gmail.com>
2022-04-13 10:00:42 +0700

    SPA Feature

---
+++ src/data/charities.js
@@ -0,0 +1,15 @@
+module.exports = {charities: [
+        {
+            id: 1,
+            title: 'First charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: +new DataTransfer('10 August 2020'),
+            profile_photo:  'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
